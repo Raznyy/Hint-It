a982c7bdb5a8999bebb173828fe45c3e02d43a59
Griva <griva199@gmail.com>
2018-05-27 19:03:50 +0200

    Added routing
    Created routes and PageNotFound component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserProfileScreenComponent } from './components/user-profile-screen/user-profile-screen.component';
 import { HomeScreenComponent } from './components/home-screen/home-screen.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeScreenComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: UserProfileScreenComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
     UserProfileScreenComponent,
     HomeScreenComponent,
     RegisterComponent,
@@ -18,7 +37,12 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
