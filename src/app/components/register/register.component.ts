d9f363bd818d884673640fd04545313a666f473b
4lkon <alkon612@gmail.com>
2018-05-28 23:14:40 +0200

    Added register component
    Added register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth.service'
+import { Router, Params } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+   registerForm: FormGroup;
 
-  constructor() { }
+  constructor(public authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder) 
+    {
+      this.createForm();
+    }
+
+    createForm() {
+      this.registerForm = this.fb.group({
+        email: ['', Validators.required ],
+        password: ['',Validators.required]
+      });
+    }
+
+    emailRegister(value){
+      this.authService.emailSignup(value)
+      .then(res => {
+        console.log(res);
+      }, err => {
+        console.log(err);
+      })
+    }
 
   ngOnInit() {
   }
