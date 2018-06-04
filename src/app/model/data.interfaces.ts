a841461ccca1419a62677a75794dce387c9cefa5
Griva <griva199@gmail.com>
2018-06-04 18:25:05 +0200

    Changed data interfaces

--- src/app/model/data.interfaces.ts
+++ src/app/model/data.interfaces.ts
@@ -1,9 +1,22 @@
 
+
 export interface Question {
-    key?: string;
-    title: string;
-    content: string;
-    lat: number;
-    lng: number;
-    timestamp: number;
+    key?: string; // Key provided from database
+    title: string; // Title of question
+    content: string; // Description of question
+    lat: number; // Latitude
+    lng: number; // Longtitude
+    timestamp: number; // Timestamp with time of creation
+    author: string; // Author user id
+    answers: Array<string> // List of answers
+}
+
+export interface Answer {
+    key?: string; // Key provided from database
+    content: string; // Description of answer
+    voteCount: number; // Latitude
+    upvotes: Array<string>; // List of user id's
+    downvotes: Array<string>; // List of user id's
+    timestamp: number; // Timestamp with time of creation
+    author: string; // Author user id
 }
