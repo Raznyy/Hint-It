953235033f2b24381b22f2feed8eb81bf8ac04f1
Griva <griva199@gmail.com>
2018-06-04 03:13:59 +0200

    Changed service to return observables

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,32 +1,38 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { FirebaseDatabase } from 'angularfire2';
-import { Observable } from '@firebase/util';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Question } from '../model/data.interfaces';
+import { ThenableReference } from '@firebase/database-types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  private database: FirebaseDatabase;
+  private questionsRef: AngularFireList<Question>;
+  public questions: Observable<Question[]>;
   
-  constructor(fireStore: AngularFireDatabase) {
-    this.database = fireStore.database;
+  constructor(db: AngularFireDatabase) {
+    
+    // Question list
+    this.questionsRef = db.list('questions');
+    this.questions = this.questionsRef.snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
   }
 
-  addQuestion(name: string){
-    return this.database.ref('questions/').push({
-      username: "alo nazwa",
-      email: "@3",
-      profile_picture : "sdasdasd"
+  createQuestion(title: string, content: string): ThenableReference{
+    return this.questionsRef.push({
+      title: title,
+      content: content,
+      lat: 0,
+      lng: 0,
+      timestamp: Date.now()
     });
-  }
-
-  getQuestions(){
-    // return this.database.ref('questions/').on('value', function(){}).then(function(snapshot) {
-    //   // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-    //   console.log(snapshot);
-    // });
   }
 
 }
